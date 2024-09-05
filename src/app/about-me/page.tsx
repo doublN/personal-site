0c05ce0b762f15e9b2ab292f340cc401db5404fb
doubl_n <n.newberry@hotmail.com>
2024-09-05 21:20:27 +0100

    inital commit

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>Content</p>
+    </main>
+  );
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Nathan Newberry",
+  description: "My personal website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="font-sans">
+      <body className="min-h-full flex justify-center items-center bg-gradient-to-br from-rose-950 to to-slate-950">
+        <div className="border-2 max-w-3xl w-full min-h-[45rem] flex flex-row rounded-lg dark:bg-black/30 bg-white/40 p-4">
+          <div className="w-1/3 border-r-2 pr-3">
+            <Link href="/about-me">
+              <h1 className="font-bold">Nathan Newberry</h1>
+            </Link>
+            <h2>Software Developer</h2>
+            <ul>
+              <li>
+                <Link href="/about-me">About Me</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="w-2/3 pl-3">{children}</div>
+        </div>
+      </body>
+    </html>
+  );
+}
