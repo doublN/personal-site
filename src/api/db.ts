113474b389cff0af4134c01e351f871d5991abdf
doubl_n <n.newberry@hotmail.com>
2024-09-22 20:19:28 +0100

    Create getArticles function to query db

--- src/api/db.ts
+++ src/api/db.ts
@@ -8,17 +8,40 @@ export type Article = {
   paragraphs: {
     p: Array<string>;
   };
-  tags: {
-    t: Array<string>;
+  tags: Array<string>;
+  link: {
+    icon: string;
+    url: string;
   };
-  link: string | null;
 };
 
 const client = createClient({
   url: "file:db/personal.db",
 });
 
-export const getArticles = async () => {
-  const results = await client.execute("SELECT * FROM articles");
-  console.log(results.rows);
+export const getArticles = async (
+  tags: Array<string>
+): Promise<Array<Article>> => {
+  const where = tags.map((tag, index) => {
+    if (index === 0) {
+      return `tags LIKE '%${tag}%' `;
+    } else {
+      return `OR tags LIKE '%${tag}%' `;
+    }
+  });
+
+  const results = await client.execute(`SELECT * FROM articles WHERE ${where}`);
+  const rows = results.rows;
+
+  return rows.map((row): Article => {
+    const entry: Article = <Article>{};
+
+    entry.id = row.id as string;
+    entry.header = row.header as string;
+    entry.paragraphs = JSON.parse(row.paragraphs as string);
+    entry.tags = typeof row.tags === "string" ? row.tags.split(" ") : [""];
+    entry.link = JSON.parse(row.link as string);
+
+    return entry;
+  });
 };
--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    getArticles();
+    getArticles(["about"]);
   }, []);
 
   return (
