9f57c398c7d28f8f145b6bb140e985c76ceca94e
doubl_n <n.newberry@hotmail.com>
2024-09-23 17:10:20 +0100

    create custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="flex h-full w-full justify-center items-center">
+      <h3 className="text-center">
+        Oops! Looks like this page doesn&apos;t exist. Click on one of the links
+        in the navigaion menu to get back on track 🚆
+      </h3>
+    </div>
+  );
+}
