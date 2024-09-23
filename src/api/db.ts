dd59f3d1b6f4f163d01058fcec9c67537c42b901
doubl_n <n.newberry@hotmail.com>
2024-09-23 17:08:50 +0100

    update articles query

--- src/api/db.ts
+++ src/api/db.ts
@@ -3,12 +3,10 @@
 import { createClient } from "@libsql/client";
 
 export type Article = {
-  id: string;
   header: string;
   paragraphs: {
     p: Array<string>;
   };
-  tags: Array<string>;
   link: {
     icon: string;
     url: string;
@@ -34,16 +32,16 @@ export const getArticles = async (
     }
   });
 
-  const results = await client.execute(`SELECT * FROM articles WHERE ${where}`);
+  const results = await client.execute(
+    `SELECT header, paragraphs, link FROM articles WHERE ${where} ORDER BY date`
+  );
   const rows = results.rows;
 
   return rows.map((row): Article => {
     const entry: Article = <Article>{};
 
-    entry.id = row.id as string;
     entry.header = row.header as string;
     entry.paragraphs = JSON.parse(row.paragraphs as string);
-    entry.tags = typeof row.tags === "string" ? row.tags.split(" ") : [""];
     entry.link = JSON.parse(row.link as string);
 
     return entry;
