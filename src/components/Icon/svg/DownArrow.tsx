9ef09bdc29ea7c5717e6a91a036395485f640d94
doubl_n <n.newberry@hotmail.com>
2024-10-01 20:48:26 +0100

    Add down arrow icon

---
+++ src/components/Icon/svg/DownArrow.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+export function DownArrow(): ReactNode {
+  return (
+    <svg viewBox="0 -960 960 960" fill="currentColor">
+      <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
+    </svg>
+  );
+}
